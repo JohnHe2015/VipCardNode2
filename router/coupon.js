f0782a93e0df3bbf6b4d50245f1d8d2f7f63ee77
johnHe <xiaozhemu@163.com>
2018-12-28 23:25:45 +0800

    fix coupon

--- router/coupon.js
+++ router/coupon.js
@@ -3,7 +3,41 @@ const router = express.Router();
 const common = require('./../common/common');
 
 router.post('/post',(req,res,next)=>{
-    let {ID,username,startTime,endTime,isUse,useTime,amount} = req.body;
+    let createTime = common.getTime();
+    let {id,username,startTime,endTime,isUse,useTime,amount} = req.body;
+    sequelize.transaction( (t)=>{
+        if(amount < 5000)   //单次消费不满5000元的情况
+        {
+            for(let i=0;i<2;i++)  //发放2张咖啡券
+            {
+                return req.Coupon_Model.create({
+                    id : id,
+                    username : username,
+                    startTime : startTime,
+                    endTime : endTime,
+                    isUse : isUse,
+                    useTime : useTime,
+                    createTime : createTime
+                })
+            }   
+        }
+        else
+        {
+            for(let i=0;i<6;i++)   //发放6张咖啡券
+            {
+                return req.Coupon_Model.create({
+                    id : id,
+                    username : username,
+                    startTime : startTime,
+                    endTime : endTime,
+                    isUse : isUse,
+                    useTime : useTime,
+                    createTime : createTime
+                })
+            }
+        }
+
+    }
     req.Coupon_Model.create({
         id : ID,
         username : username,
