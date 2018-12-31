427a3712fe11250d8a595241b11f644298202f2f
johnHe <xiaozhemu@163.com>
2018-12-31 17:49:53 +0800

    add wx config

---
+++ config/wx.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    wx : {
+        appID : "wxbc59dd91a972da5c",
+        appsecret : "953e390c8f7eb8ff9ef53468907f9c55",
+        redirect_url : "www.zhengshuqian.com/#/register"
+    }
+};
