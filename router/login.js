a2f1b699076da0117c9f54c087948d7fa6508255
johnHe <xiaozhemu@163.com>
2019-01-01 15:15:17 +0800

    wx

--- router/login.js
+++ router/login.js
@@ -5,7 +5,7 @@ const common = require('./../common/common');
 router.get('/isLogin',(req,res,next)=>{    //判断微信用户是否存在，存在则直接跳转到card页面
     console.log('come in isLogin');
     console.log(JSON.stringify(req.query));
-    let {id} = req.query.id;
+    let id = req.query.id;
     req.User_Model.findOne({ where: {id: id}}).then(data =>{
         console.log(JSON.stringify(data));
         if(data.length > 0)
