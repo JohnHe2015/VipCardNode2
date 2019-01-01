2ac559475e6a67dd16cf0e5fd0a9afbd2d8d7d69
johnHe <xiaozhemu@163.com>
2019-01-01 22:52:59 +0800

    wx

--- router/coupon.js
+++ router/coupon.js
@@ -123,7 +123,7 @@ router.get('/getById/:id',(req,res,next)=>{
                 headers: {
                     "content-type": "application/json",
                 },
-                body: {data : JSON.stringify(data)}
+                body: {data : data}
             }, function(error, response, body) {
                 if (!error && response.statusCode == 200) {
                     console.log('传递coupon数据成功！')
