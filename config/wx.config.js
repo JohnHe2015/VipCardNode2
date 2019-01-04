d2687b6a107a6b4efd1efba4222afc702fe02850
JohnHe2015 <xiaozhemu@163.com>
2019-01-04 11:29:14 +0800

    wx

--- config/wx.config.js
+++ config/wx.config.js
@@ -3,6 +3,6 @@ module.exports = {
         appID : "wxbc59dd91a972da5c",
         appsecret : "953e390c8f7eb8ff9ef53468907f9c55",
         redirect_url : "www.zhengshuqian.com/#/register",
-        token : '58452011'
+        token : '584520111'
     }
 };
