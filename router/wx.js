e72fe27bbcd1029e826e7ecd6775763440a5522b
johnHe <xiaozhemu@163.com>
2019-01-05 20:40:45 +0800

    wx

--- router/wx.js
+++ router/wx.js
@@ -285,7 +285,7 @@ router.all('/eventTrigger',(req,res,next)=>{   //既接收get也接收事件post
                             console.log('response'+response);
                             console.log('body'+body);
                             if(response.statusCode == 200){
-                                if(body.errcode == 0 )   //核销成功
+                                if(body.errcode == "0" )   //核销成功
                                 {
                                     console.log('errcode == 0');
                                     res.redirect('http://m.zhengshuqian.com/coupon/result');
